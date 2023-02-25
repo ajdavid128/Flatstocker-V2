f4962cf8737e3a0848bfc88046a0e4e2a257d4ba
Aaron David <ajdavid128@gmail.com>
2023-02-25 13:20:39 -0600

    reverted back to having the login form being imported and the signupform button being commented out, still have the new toggle state, but waiting to implement that until I decide how the login/signup form switch will work

--- client/src/Components/LandingPage.js
+++ client/src/Components/LandingPage.js
@@ -1,6 +1,6 @@
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { useState } from "react";
-import { Button, Form, Input, Container, Divider, Image, Segment } from "semantic-ui-react";
+import { Button, Container, Divider, Image, Segment } from "semantic-ui-react";
 import LoginForm from "./LoginForm";
 import SignupForm from "./SignupForm";
 import FlatStocker from "../images/FlatStocker.png";
@@ -9,114 +9,13 @@ import Print_Pins from "../images/Print_Pins.png";
 
 function LandingPage({errors, setErrors, currentUser, setCurrentUser}) {
 
-    let navigate = useNavigate();
-
-    // **************************************************************** 
-    // STATE CODE
-    // **************************************************************** 
-
-    const [toggleLoginSignup, setToggleLoginSignup] = useState(true);
-    const [newUser, setNewUser] = useState({
-        name: "",
-        email: "",
-        business_name: "",
-        username: "",
-        password: "",
-        password_confirmation: ""
-    })
-    const [user, setUser] = useState({
-        username: "",
-        password: ""
-    });
-
-    // **************************************************************** 
-    // LOGIN CODE
-    // **************************************************************** 
-
-    const handleLoginChange = (e) => {
-        const key = e.target.name;
-        const value = e.target.value;
-
-        setUser({
-            ...user, [key]: value
-        })
-    };
-
-    const handleLogin = (e) => {
-        e.preventDefault();
-        fetch("/login", {
-            method: "POST",
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify(user)
-        })
-        .then(res =>{
-            if(res.ok){
-                res.json().then(user => {
-                    setCurrentUser(user);
-                    navigate("/dashboard");
-                })
-            } else {
-                res.json().then(data =>{
-                    for (const key in data){
-                        setErrors(data[key]);
-                    }
-                })
-            }
-        });
-        setUser({
-            username: "",
-            password: ""
-        });
-    };
-
-    // **************************************************************** 
-    // SIGNUP CODE
-    // **************************************************************** 
-
-    const handleSignup = (e) => {
-        e.preventDefault();
-        fetch("/signup", {
-            method: "POST",
-            headers: {"Content-Type": "application/json"},
-            body: JSON.stringify(newUser)
-        })
-        .then(res => {
-            if(res.ok) {
-                res.json().then(data => {
-                    setCurrentUser(data);
-                    navigate("/dashboard");
-                })
-            } else {
-                res.json().then(data => setErrors(data.errors));
-            }
-        })
-        setNewUser({
-            name: "",
-            email: "",
-            business_name: "",
-            username: "",
-            password: "",
-            password_confirmation: ""
-        })
-    };
-
-    const handleSignupChange = (e) => {
-        const key = e.target.name;
-        const value = e.target.value;
-
-        setNewUser({
-            ...newUser, [key]: value
-        })
-    };
+    const [toggleLoginSignup, setToggleLoginSignup] = useState(false)
 
     const handleClearError = () => {
-        setToggleLoginSignup(!toggleLoginSignup);
         setErrors([]);
     }
     
     return (
-        <>
-        {toggleLoginSignup ?
         <div id="background-login">
 
         <div id="landing-page">
@@ -130,43 +29,11 @@ function LandingPage({errors, setErrors, currentUser, setCurrentUser}) {
             <div >
                 <div id="land-container">
                     <Segment id="land-seg">
-                        {/* <LoginForm 
+                        <LoginForm 
                             setCurrentUser={setCurrentUser}
                             errors={errors}
                             setErrors={setErrors}
-                        /> */}
-                        <Container>
-                            <h1 id="login-title" >Please Login</h1>
-                            <Form onSubmit={handleLogin}>
-                                <Form.Field 
-                                    control={Input}
-                                    label="Username:"
-                                    name="username"
-                                    placeholder="Username"
-                                    value={user.username}
-                                    onChange={handleLoginChange}
-                                />
-                                <Form.Field 
-                                    control={Input}
-                                    label="Password:"
-                                    type="password"
-                                    name="password"
-                                    placeholder="Password"
-                                    value={user.password}
-                                    onChange={handleLoginChange}
-                                />
-                                <div>
-                                    {errors && errors? <p className="error"> {errors}</p> : null}
-                                {/* {errors && errors? errors.map(e => { return <p className="error" key={e}>• {e}</p>}) : null} */}
-                                </div>
-                                <div id="login-form-button">
-                                    <Form.Button type="submit">Login</Form.Button>
-                                </div>
-                                
-                            </Form>
-
-                        </Container>
-
+                        />
                         <Divider horizontal>OR</Divider>
                         <div id="sign-up-about">
                             <div className="sign-about-buttons">
@@ -188,100 +55,9 @@ function LandingPage({errors, setErrors, currentUser, setCurrentUser}) {
                         </div>
                     </Segment>
                 </div>
-            </div>   
+            </div>  
         </div>
         </div>
-
-        :
-        
-        // <SignupForm/>
-        <div id="signup-page">
-
-            <div>
-                <div id="landing-logo">
-                    <Segment id="logo-seg">
-                        <Image src={FlatStocker} size="huge"/>
-                    </Segment>
-                </div>
-
-                <div id="signup-cont">               
-                    <Segment id="signup-seg">
-                        <h1 id="signup-title">Please Sign Up</h1>
-                        <Form onSubmit={handleSignup}>
-                            <Form.Field 
-                                control={Input}
-                                label="Full Name:"
-                                type="name"
-                                name="name"
-                                placeholder="Jon Smith"
-                                value={newUser.name}
-                                onChange={handleSignupChange}
-                            />
-                            <Form.Field 
-                                control={Input}
-                                label="Email:"
-                                type="text"
-                                name="email"
-                                placeholder="print@example.com"
-                                value={newUser.email}
-                                onChange={handleSignupChange}
-                            />
-                            <Form.Field 
-                                control={Input}
-                                label="Business Name:"
-                                type="text"
-                                name="business_name"
-                                placeholder="Print Shop"
-                                value={newUser.business_name}
-                                onChange={handleSignupChange}
-                            />
-                            <Form.Field 
-                                control={Input}
-                                label="Username:"
-                                type="text"
-                                name="username"
-                                placeholder="anythingYoUw4nT!"
-                                value={newUser.username}
-                                onChange={handleSignupChange}
-                            />
-                            <Form.Field 
-                                control={Input}
-                                label="Password:"
-                                type="password"
-                                name="password"
-                                value={newUser.password}
-                                onChange={handleSignupChange}
-                            />
-                            <Form.Field 
-                                control={Input}
-                                label="Password Confirmation:"
-                                type="password"
-                                name="password_confirmation"
-                                value={newUser.password_confirmation}
-                                onChange={handleSignupChange}
-                            />
-                            <div>
-                                {errors && errors? errors && errors.map(e => { return <p className="error" key={e}>• {e}</p>}) : null}
-                            </div>
-                            <div id="create-account-button">
-                                <Form.Button type="submit">Create Account</Form.Button>
-                            </div>
-                        </Form>
-                
-                        <Divider horizontal>OR</Divider>
-                    
-                        
-                        <div id="signup-form-login-button">
-                            <Button onClick={handleClearError}>Login</Button>
-                        </div>
-                    </Segment>
-                    
-                </div>
-            </div>
-
-        </div>
-        }
-        </>
     )
 };
 
