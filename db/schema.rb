67ce16a802362c36fde48cae575d4a050c8fcd69
Aaron David <ajdavid128@gmail.com>
2023-03-26 13:24:37 -0500

    added migration to retails for user_id

--- db/schema.rb
+++ db/schema.rb
@@ -44,15 +44,6 @@ ActiveRecord::Schema.define(version: 2023_02_10_224915) do
     t.bigint "user_id"
   end
 
-  create_table "user_retailers", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "retailer_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["retailer_id"], name: "index_user_retailers_on_retailer_id"
-    t.index ["user_id"], name: "index_user_retailers_on_user_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -65,6 +56,4 @@ ActiveRecord::Schema.define(version: 2023_02_10_224915) do
 
   add_foreign_key "items", "retailers"
   add_foreign_key "items", "users"
-  add_foreign_key "user_retailers", "retailers"
-  add_foreign_key "user_retailers", "users"
 end
